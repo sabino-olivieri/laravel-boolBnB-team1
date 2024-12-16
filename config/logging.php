f9a4ac22d35bc82cf6fa8d1f2e0039e9471cfbd7
sabino-olivieri <sabino.olivieri.28@gmail.com>
2024-12-16 12:52:57 +0100

    Revert "use errorlog"
    This reverts commit 4084af0923ea591408e53832db5b2da892840bcb.

--- config/logging.php
+++ config/logging.php
@@ -55,11 +55,14 @@ return [
         'stack' => [
             'driver' => 'stack',
             'channels' => ['single'],
+            'ignore_exceptions' => false,
         ],
 
         'single' => [
-            'driver' => 'errorlog',
-            'level' => 'debug',
+            'driver' => 'single',
+            'path' => storage_path('logs/laravel.log'),
+            'level' => env('LOG_LEVEL', 'debug'),
+            'replace_placeholders' => true,
         ],
 
         'daily' => [
